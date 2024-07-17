e4717cb3b86ee5c98e3edecbaa2cc430ba262ac0
Phone Htut <phonehtutkhaung.dev@gmail.com>
2024-07-17 19:22:59 +0630

    first commit in migration table

---
+++ database/migrations/2024_07_17_121645_create_mangas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->longText('content');
+            $table->string('image');
+            $table->string('author')->nullable();
+            $table->foreignId('user_id')->constrained()->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
---
+++ database/migrations/2024_07_17_121656_create_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->foreignId('manga_id')->constrained()->on('mangas')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
